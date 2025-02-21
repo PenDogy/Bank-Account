3ba04168f201dc3143af8f44eeb29998c72513a5
unknown <popo181138@gmail.com>
2025-02-21 11:33:10 +0700

    Detail b3

--- Main.java
+++ Main.java
@@ -2,15 +2,19 @@ public class Main{
     public static void main(String[] args) {
         BankAccount b1 = new BankAccount("Dan", 19, 725, 500);
         BankAccount b2 = new BankAccount("Woody", 19, 730, 1500);
+        BankAccount b3 = new BankAccount("cap", 19, 123, 1200);
 
         BankAccount.ShowAccount();
 
         b1.showBalance();
         b2.showBalance();
+        b3.showBalance();
 
         BankAccount.transfer(b1, b2, 500);
+        BankAccount.transfer(b3, b1, 1000);
 
         b1.showBalance();
         b2.showBalance();
+        b3.showBalance();
     }
 }
